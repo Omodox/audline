1c34ec58a2f78263f151375bf3fa9ee2381f6c17
Denis Khoroshcho <khoroshcho@gmail.com>
2018-05-20 10:54:35 +0300

    Analitics !!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,25 @@
-import { Component, Output, OnInit, HostListener } from '@angular/core';
+import {Component} from "@angular/core";
+import {Router, NavigationEnd} from "@angular/router";
 
+declare let ga: Function;
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  constructor( ) { }
-
-
-
-  ngOnInit() {
-
-    
+  title = 'app works!';
+  
+  constructor(public router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        
+        setTimeout( function(){
+          ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+        },1000);
+      }
+    });
   }
-
-
-
-
 }
