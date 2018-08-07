32e43afbaf6deaf8e0af86870214b3628424d6c5
Denis Khoroshcho <khoroshcho@gmail.com>
2018-08-07 17:07:24 +0300

    btnad

--- src/app/band/band.component.ts
+++ src/app/band/band.component.ts
@@ -63,6 +63,12 @@ export class BandComponent implements OnInit {
 
 
 
+    (function(){
+      var D=new Date(),d=<any>document,b='body',ce='createElement',ac='appendChild',st='style',ds='display',n='none',gi='getElementById',lp=d.location.protocol,wp=lp.indexOf('http')==0?lp:'https:';
+      var i=d[ce]('iframe');i[st][ds]=n;d[gi]("M403150ScriptRootC668360")[ac](i);try{var iw=i.contentWindow.document;iw.open();iw.writeln("<ht"+"ml><bo"+"dy></bo"+"dy></ht"+"ml>");iw.close();var c=iw[b];}
+      catch(e){var iw=d;var c=d[gi]("M403150ScriptRootC668360");}var dv=iw[ce]('div');dv.id="MG_ID";dv[st][ds]=n;dv.innerHTML=668360;c[ac](dv);
+      var s=iw[ce]('script');s.async='async';s.defer='defer';s.charset='utf-8';s.src=wp+"//jsc.marketgid.com/a/u/audline.net.668360.js?t="+D.getFullYear()+D.getMonth()+D.getUTCDate()+D.getUTCHours();c[ac](s);})();
+
   }
 
 }
